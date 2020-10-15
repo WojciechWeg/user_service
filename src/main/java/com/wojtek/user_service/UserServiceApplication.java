a9000378aa410cc8d66fa9795efe5406172c441a
WojciechWeg <wojtek.weg@gmail.com>
2020-10-15 23:09:05 +0200

    add eureka support

--- src/main/java/com/wojtek/user_service/UserServiceApplication.java
+++ src/main/java/com/wojtek/user_service/UserServiceApplication.java
@@ -1,10 +1,21 @@
 package com.wojtek.user_service;
 
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
 
 @SpringBootApplication
 public class UserServiceApplication {
+
+    @Autowired
+    @Lazy
+    private EurekaClient eurekaClient;
+
+    @Value("${spring.application.name}")
+    private String appName;
 
     public static void main(String[] args) {
         SpringApplication.run(UserServiceApplication.class, args);
